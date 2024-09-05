db5b72d0c7112eec9ba1eac246dd4ec0844182ea
Z <zakcrowson@hotmail.com>
2024-09-05 22:32:18 +0100

    Fixed c_loc.csv write for cotyledon whose edges aren't consecutively ordered, added plot function to output basic image of geometry info

--- placenta_plots.py
+++ placenta_plots.py
@@ -26,8 +26,8 @@ def plot_placentone_list(points,placentone_list):
     
     #ax.plot(0.0, 0.0, c='C0', lw=10, label="Blue signal", zorder=10)
     #ax.plot(0.0, 1.0, c='C1', lw=10, label="Orange signal")
-    ax.plot([-placenta_radius,placenta_radius,placenta_radius,-placenta_radius,-placenta_radius], \
-            [-placenta_radius,-placenta_radius,placenta_radius,placenta_radius,-placenta_radius], zorder=2, color='black', linewidth=1, label="Orange signal")
+    #ax.plot([-placenta_radius,placenta_radius,placenta_radius,-placenta_radius,-placenta_radius], \
+    #        [-placenta_radius,-placenta_radius,placenta_radius,placenta_radius,-placenta_radius], zorder=2, color='black', linewidth=1, label="Orange signal")
     c = matplotlib.patches.Circle((0.0, 0.0), radius=placenta_radius, clip_on=False, zorder=1, linewidth=1.0,
                edgecolor='black', facecolor='none',
                path_effects=[matplotlib.patheffects.withStroke(linewidth=5, foreground='white')])
@@ -149,6 +149,76 @@ def plot_edge_set(edge_set,placentone_list = None):
                     ha='center', va='top', weight='bold', color='orange',
                     fontfamily='Courier New',fontsize=16)
             
+    plt.show()
+    
+    return None
+
+def plot_cotyeldon_lobule_list(points, cotyeldon_list, lobule_list, \
+        cotyledon_artery_number = None, cotyledon_vein_number = None):
+
+    shift = placenta_voronoi_outer_radius_offset
+    fig = plt.figure()
+    ax = fig.add_axes([1,1,2,2], aspect=1)
+    ax.set_xlim(-placenta_radius-shift,placenta_radius+shift)
+    ax.set_ylim(-placenta_radius-shift,placenta_radius+shift)
+    
+    #ax.plot(0.0, 0.0, c='C0', lw=10, label="Blue signal", zorder=10)
+    #ax.plot(0.0, 1.0, c='C1', lw=10, label="Orange signal")
+    c = matplotlib.patches.Circle((0.0, 0.0), radius=placenta_radius, clip_on=False, zorder=1, linewidth=1.0,
+               edgecolor='black', facecolor='none',
+               path_effects=[matplotlib.patheffects.withStroke(linewidth=5, foreground='white')])
+    ax.add_artist(c)    
+    
+    # Cotyledon
+    for placentone_no,placentone in enumerate(cotyeldon_list):
+        placentone_no_vertices = placentone.no_vertices
+        placentone_no_edges = placentone.no_edges
+        placentone_vertices = numpy.copy(placentone.vertices)
+        placentone_edges = numpy.copy(placentone.edges)
+        placentone_centroid = numpy.copy(placentone.centroid)
+        
+        ax.text(placentone_centroid[0] + 0.1*abs(placentone_centroid[0]), placentone_centroid[1], \
+            str(placentone_no + 1), zorder=10,
+            ha='center', va='top', weight='bold', color='red',
+            fontfamily='Courier New',fontsize=24)
+        
+        ax.scatter(*placentone_centroid, \
+            zorder=8, color='red')
+        
+        for edge_no in range(0,placentone_no_edges):
+            X = numpy.array([placentone_vertices[placentone_edges[edge_no,0],0],placentone_vertices[placentone_edges[edge_no,1],0]])
+            Y = numpy.array([placentone_vertices[placentone_edges[edge_no,0],1],placentone_vertices[placentone_edges[edge_no,1],1]])
+            edge_c = numpy.array([(X[1]+X[0])/2.0,(Y[1]+Y[0])/2.0])
+            ax.plot(X,Y,c='g',zorder=9) 
+            
+        if (cotyledon_artery_number is not None):
+            ax.text(placentone_centroid[0] + 0.1*placenta_radius, \
+                placentone_centroid[1] + 0.1*placenta_radius, \
+                f"Arteries: {str(cotyledon_artery_number[placentone_no])}", zorder=10,
+                ha='center', va='top', weight='bold', color='blue',
+                fontfamily='Courier New',fontsize=18)
+        if (cotyledon_vein_number is not None):
+            ax.text(placentone_centroid[0] + 0.1*placenta_radius, \
+                placentone_centroid[1] - 0.1*placenta_radius, \
+                f"Veins: {str(cotyledon_vein_number[placentone_no])}", zorder=10,
+                ha='center', va='top', weight='bold', color='blue',
+                fontfamily='Courier New',fontsize=18)
+    
+    # Lobules
+    for lobule_no,lobule in enumerate(lobule_list):
+        lobule_no_vertices = lobule.no_vertices
+        lobule_no_edges = lobule.no_edges
+        lobule_vertices = numpy.copy(lobule.vertices)
+        lobule_edges = numpy.copy(lobule.edges)
+        lobule_centroid = numpy.copy(lobule.centroid)
+        
+        for edge_no in range(0,lobule_no_edges):
+            X = numpy.array([lobule_vertices[lobule_edges[edge_no,0],0],lobule_vertices[lobule_edges[edge_no,1],0]])
+            Y = numpy.array([lobule_vertices[lobule_edges[edge_no,0],1],lobule_vertices[lobule_edges[edge_no,1],1]])
+            edge_c = numpy.array([(X[1]+X[0])/2.0,(Y[1]+Y[0])/2.0])
+            ax.plot(X,Y,c='g',zorder=19, alpha = 0.1)    
+    
+    
     plt.show()
     
     return None
